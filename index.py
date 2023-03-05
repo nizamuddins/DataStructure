1821cfddb7f8ff60372335fb2cb474c1236fa59a
nizamuddins <uddinmdnizam61@gmail.com>
2023-03-06 00:00:37 +0530

    dsa

---
+++ index.py
@@ -0,0 +1,32 @@
+# DataStructure
+
+#  A DataStructure is used to organize,manage and store data in specific format.
+
+# Types of DataStructure
+# 1)primitive
+# 2)non-primitive
+
+# 1)primitive
+# a)integer
+# b)Character
+# c)Float
+# d)String
+# e)Double
+
+# 2)Non-primitive
+# a)Linear
+# b)Non-Linear
+
+# a)Linear
+# 1)Array
+# 2)Queue
+# 3)Stack
+# 4)Linkedlist
+
+# b)Non-Linear
+# 1)Tree
+# 2)Graph
+
+# Array
+
+A
