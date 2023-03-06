32620fc910ae38fb4a1866d6b236137c5f4b34a7
nizamuddins <uddinmdnizam61@gmail.com>
2023-03-06 23:19:28 +0530

    dsa

--- index.py
+++ index.py
@@ -1,3 +1,56 @@
+# Time complexity => # The process of analysing time complexity of algorithm or programm as it vaeries on input
+
+# rate of growth => # rate of time increases as the function of input is known as rate of growth
+
+# commonly rate of growth:
+
+# 1 => constant
+# logn => logarthamic
+# n => Linear
+# nlogn => Linear logarthamic
+# (n)2 => Quadratic
+# (n)3 => Cubic
+# (2)n => exponential
+# n! => factorial
+
+# Types of Analysis => 
+
+# worst-case => Big O notation(O)
+# Best-case => Omega notation(omega)
+# Average-case => Theta notation(Theta)
+
+# Asymptotic Notations => It is used to determine the upper-bound and lower-bond
+
+# Types of Notations:
+
+# => Big O notation(O)
+# => Omega notation(omega)
+# => Theta notation(Theta)
+
+# Time complexity based on loops
+
+#single for loop:
+
+# for i in range(0,5): |=>O(n)
+#     print(i)         |
+
+# nested for loop:
+
+# for i in range(0,5):   |
+#   for i in range(0,j): |=>O((n)2)
+#     print(i)         | |
+        |
+
+#consecutive for loop:
+
+# for i in range(0,5):   |
+#     print(i)           |
+#                        |=>O((n)2)
+# for i in range(0,5):   |
+#   for i in range(0,j): |
+#     print(i)         | |
+        |
+
 # DataStructure
 
 #  A DataStructure is used to organize,manage and store data in specific format.
@@ -6,27 +59,30 @@
 # 1)primitive
 # 2)non-primitive
 
-# 1)primitive
+# 1)primitive=>
+
 # a)integer
 # b)Character
 # c)Float
 # d)String
 # e)Double
 
-# 2)Non-primitive
+# 2)Non-primitive=>
+
 # a)Linear
 # b)Non-Linear
 
-# a)Linear
+# a)Linear=>
+
 # 1)Array
 # 2)Queue
 # 3)Stack
 # 4)Linkedlist
 
-# b)Non-Linear
+# b)Non-Linear=>
+
 # 1)Tree
 # 2)Graph
 
-# Array
 
-A
+
